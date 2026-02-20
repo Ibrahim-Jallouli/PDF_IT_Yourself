7e26221cc8f1dd048c8b17bb9d89b16ac8ca067e
Ibrahim-Jallouli <ibrahim.jallouli@alumni.univ-avignon.fr>
2026-02-20 14:50:02 +0100

    Changement nom projet

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using PDF_Yourself;
+using PDF_IT_Yourself;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
